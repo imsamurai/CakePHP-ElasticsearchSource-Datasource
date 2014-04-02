ed80c95693416260280e23c0cbd6a87410c310e0
Aleksandr <skuridin-alex@ya.ru>
2014-04-02 18:32:55 +0300

    fix affected rows count, prepare log for DebugKit

--- Model/Datasource/Http/ElasticsearchSource.php
+++ Model/Datasource/Http/ElasticsearchSource.php
@@ -27,6 +27,11 @@ class ElasticsearchSource extends HttpSource {
 	const HTTP_METHOD_UPDATE = 'PUT';
 
 	/**
+	 * Maximum log length
+	 */
+	const LOG_MAX_LENGTH = 1000;
+
+	/**
 	 * Elasticsearch API Datasource
 	 *
 	 * @var string
@@ -88,7 +93,9 @@ class ElasticsearchSource extends HttpSource {
 					$this->_requestStatus[$requestMethod][$indexName] = $result[$indexName];
 				}
 			if (isset($result['hits'])) {
-				$this->affected = count($result['hits']);
+				if(isset($result['hits']['hits'])){
+					$this->affected = count($result['hits']['hits']);
+				}
 				foreach ($gethits as $indexName)
 					if (isset($result['hits'][$indexName])) {
 						$this->_requestStatus[$requestMethod][$indexName] = $result['hits'][$indexName];
@@ -130,4 +137,37 @@ class ElasticsearchSource extends HttpSource {
 		parent::logRequest();
 	}
 
+	/**
+	 * Log given query.
+	 *
+	 * @return void
+	 */
+	public function logPrepare() {
+		$log = array(
+			'query' => strlen($this->query) > static::LOG_MAX_LENGTH ? substr($this->query, 0, static::LOG_MAX_LENGTH) . ' ' . static::LOG_TRUNCATED : $this->query,
+			'affected' => $this->affected,
+			'numRows' => $this->numRows,
+			'took' => $this->took,
+		);
+		$http_parts = explode('HTTP/', $this->query);
+
+		$get_parts = explode('GET ', $http_parts[0]);
+		if(isset($get_parts[1]) && isset($http_parts[1])){
+			$host_parts = explode('Host:', $http_parts[1]);
+			$request_parts = explode('Content-Length: ', $http_parts[1]);
+			$http_request = '';
+			if(isset($request_parts[1]) && false !== strpos($request_parts[1], '{')){
+				$http_request = strstr($request_parts[1], '{');
+			}
+			$connection_parts = explode('Connection:', $host_parts[1]);
+			$get_request = trim($get_parts[1]);
+
+			$http_request = $get_request.(false !== strpos($get_request, '?') ? '&source=' : '?source=').htmlentities($http_request);
+			$explain_request = $http_request.'&explain=true';
+
+			//$log['took'] .= '<br/><a href="http://'.trim($connection_parts[0]).$explain_request.'"><b>Explain</b></a>';
+			//$log['took'] .= '<br/><a href="http://'.trim($connection_parts[0]).$http_request.'"><b>Request</b></a>';
+		}
+		$this->_logRow = $log;
+	}
 }
