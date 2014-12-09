caf13c6c7e4083d712e1e9d416391ab427cad5a5
imsamurai <imsamurai@imsamurai-2>
2014-12-09 14:42:21 +0200

    change travis

--- travis/after_success.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$PHPDOC" = 1 ]; then
-	./travis/phpdoc_after_success.sh;
-fi;
-if [ "$PHPDOC" != 1 ]; then
-	../travis/after_success.sh
-fi;
--- travis/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$PHPDOC" = 1 ]; then
-	./travis/phpdoc.sh;
-fi;
-if [ "$PHPDOC" != 1 ]; then
-	../travis/script.sh
-fi;
